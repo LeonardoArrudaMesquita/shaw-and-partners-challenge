f89bbe274a6299da8cdb0e66da3de09155bd5efa
Leonardo Mesquita <LeonardoArrudaMesquita>
2023-08-13 21:08:21 -0300

    chore: add react router dom

---
+++ frontend/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Routes as RouterDOMRoutes,
+  Route,
+} from "react-router-dom";
+
+import App from "../src/App";
+import React from "react";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <RouterDOMRoutes>
+        <Route path="/" element={<App />} />        
+      </RouterDOMRoutes>
+    </BrowserRouter>
+  );
+}
